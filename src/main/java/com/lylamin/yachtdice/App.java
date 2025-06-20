475228cd21a63b808837990a5fa63e78a6e8ec77
Lyla <mjuudev@gmail.com>
2025-06-20 16:55:48 -0400

    feat: initialize the project with maven

---
+++ src/main/java/com/lylamin/yachtdice/App.java
@@ -0,0 +1,13 @@
+package com.lylamin.yachtdice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
